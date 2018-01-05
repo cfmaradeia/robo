ae0829aea890ea1a69a7a616d093fb4b79e4fccb
cfmaradeia <cfmaradeia@gmail.com>
2018-01-05 00:32:48 -0200

    Criação do projeto com springboot

---
+++ src/main/java/com/contaazul/robo/RoboApplication.java
@@ -0,0 +1,12 @@
+package com.contaazul.robo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboApplication.class, args);
+	}
+}
---
+++ src/main/java/com/contaazul/robo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.contaazul.robo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RoboApplication.class);
+	}
+
+}
